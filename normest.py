3715603dec9226d8c73edc1e4d53cc0c1e425e34
pericycle <shelveankapita@gmail.com>
2022-04-11 23:02:23 +0200

    Create normest.py

---
+++ normest.py
@@ -0,0 +1,19 @@
+def normest(M,tol):
+    # return an approximation of the 2 norm of a matrix
+    # M is a numpy array or a scipy csr_matrix
+    # up to a tolerance of tol using the power method
+    max_it = 25
+    res = 1
+    it_count = 0
+    x = np.random.rand(M.shape[1],1)
+    y = M.dot(x)
+    pnorm = np.sqrt(np.sum(y**2))
+    x = y/pnorm
+    while (res>tol)&(it_count<max_it):
+        y = M.dot(x)
+        ynorm = np.sqrt(np.sum(y**2))
+        res = abs(pnorm-ynorm)
+        x = y/ynorm
+        it_count += 1
+    v = M.dot(x)
+    return np.sqrt(np.sum(v**2))
